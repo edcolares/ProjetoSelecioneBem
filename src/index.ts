768a6c06994f131ae859b7e3d9c9d0ed7a395b25
Alexsandro Tirloni <tirloni.rs@gmail.com>
2023-04-15 23:55:17 -0300

    Configuração básica

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import { AppDataSource } from './data-source'
+//import routes from './routes'
+import cors from 'cors'
+
+AppDataSource.initialize().then(() => {
+    const app = express()
+
+    app.use(cors())
+    app.use(express.json())
+
+    //app.use(routes)
+
+  /*  app.get('/', (req, res) => {
+        return res.json('Tudo certo!')
+    })
+*/
+    return app.listen(process.env.PORT, () => {
+        console.log(`🚀 🆙 Server is running on http://localhost:${process.env.PORT}`)
+    })
+
+})
